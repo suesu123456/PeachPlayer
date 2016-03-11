4b9b98d80af08ef93a3b15823922f5d76b125926
yxk <suesu123456@gmail.com>
2016-03-11 17:12:51 +0800

    third add

---
+++ PeachPlayer/MBProgressHUD+Add.m
@@ -0,0 +1,139 @@
+//
+//  MBProgressHUD+Add.m
+//  视频客户端
+//
+//  Created by mj on 13-4-18.
+//  Copyright (c) 2013年 itcast. All rights reserved.
+//
+
+#import "MBProgressHUD+Add.h"
+
+@implementation MBProgressHUD (Add)
+
+#pragma mark 显示信息
++ (void)show:(NSString *)text icon:(NSString *)icon view:(UIView *)view
+{
+    if (view == nil) view = [UIApplication sharedApplication].keyWindow;
+    // 快速显示一个提示信息
+    MBProgressHUD *hud = [MBProgressHUD showHUDAddedTo:view animated:YES];
+    hud.labelText = text;
+    // 设置图片
+    hud.customView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:[NSString stringWithFormat:@"MBProgressHUD.bundle/%@", icon]]];
+    // 再设置模式
+    hud.mode = MBProgressHUDModeCustomView;
+    
+    // 隐藏时候从父控件中移除
+    hud.removeFromSuperViewOnHide = YES;
+    
+    // 1秒之后再消失
+    [hud hide:YES afterDelay:1];
+}
++ (void)show:(NSString *)text view:(UIView *)view
+{
+    if (view == nil) view = [UIApplication sharedApplication].keyWindow;
+    // 快速显示一个提示信息
+    MBProgressHUD *hud = [MBProgressHUD showHUDAddedTo:view animated:YES];
+    hud.labelText = text;
+    hud.labelFont = [UIFont systemFontOfSize:13];
+    hud.yOffset = [UIScreen mainScreen].bounds.size.height / 3;
+    hud.userInteractionEnabled = NO;
+    // 设置图片
+    // 再设置模式
+    hud.mode = MBProgressHUDModeText;
+    
+    // 隐藏时候从父控件中移除
+    hud.removeFromSuperViewOnHide = YES;
+    
+    // 2秒之后再消失
+    [hud hide:YES afterDelay:2];
+}
+
+
+#pragma mark 显示错误信息
++ (void)showError:(NSString *)error toView:(UIView *)view{
+    [self show:error icon:@"error.png" view:view];
+}
+
++ (void)showSuccess:(NSString *)success toView:(UIView *)view
+{
+    [self show:success icon:@"success.png" view:view];
+}
++ (void)showWarn:(NSString *)warn toView:(UIView *)view
+{
+    [self show:warn view:view];
+}
+
+#pragma mark 显示一些信息
++ (MBProgressHUD *)showMessag:(NSString *)message toView:(UIView *)view {
+    if (view == nil) view = [UIApplication sharedApplication].keyWindow;
+    // 快速显示一个提示信息
+    MBProgressHUD *hud = [MBProgressHUD showHUDAddedTo:view animated:YES];
+    hud.labelText = message;
+    // 隐藏时候从父控件中移除
+    hud.removeFromSuperViewOnHide = YES;
+    // YES代表需要蒙版效果
+    hud.dimBackground = YES;
+    return hud;
+}
+
+
++ (void)hide:(UIView *)view {
+    if (view == nil) view = [UIApplication sharedApplication].keyWindow;
+    // 快速显示一个提示信息
+    [MBProgressHUD hideHUDForView:view animated:YES];
+}
+
+
+
++ (NSString *)tipFromError:(NSError *)error{
+    if (error && error.userInfo) {
+        NSMutableString *tipStr = [[NSMutableString alloc] init];
+        if ([error.userInfo objectForKey:@"msg"]) {
+            NSArray *msgArray = [[error.userInfo objectForKey:@"msg"] allValues];
+            NSUInteger num = [msgArray count];
+            for (int i = 0; i < num; i++) {
+                NSString *msgStr = [msgArray objectAtIndex:i];
+                if (i+1 < num) {
+                    [tipStr appendString:[NSString stringWithFormat:@"%@\n", msgStr]];
+                }else{
+                    [tipStr appendString:msgStr];
+                }
+            }
+        }else{
+            if ([error.userInfo objectForKey:@"NSLocalizedDescription"]) {
+                tipStr = [error.userInfo objectForKey:@"NSLocalizedDescription"];
+            }else{
+                [tipStr appendFormat:@"ErrorCode%ld", (long)error.code];
+            }
+        }
+        return tipStr;
+    }
+    return nil;
+}
+
++ (void)showError:(NSError *)error{
+    //    if ([JDStatusBarNotification isVisible]) {//如果statusBar上面正在显示信息，则不再用hud显示error
+    //        NSLog(@"如果statusBar上面正在显示信息，则不再用hud显示error");
+    //        return;
+    //    }
+    NSString *tipStr = [self tipFromError:error];
+    [self showHudTipStr:tipStr];
+}
+
+
++ (void)showHudTipStr:(NSString *)tipStr{
+    if (tipStr && tipStr.length > 0) {
+        MBProgressHUD *hud = [MBProgressHUD showHUDAddedTo:[UIApplication sharedApplication].keyWindow animated:YES];
+        hud.mode = MBProgressHUDModeText;
+        hud.labelText = tipStr;
+        hud.margin = 10.f;
+        hud.removeFromSuperViewOnHide = YES;
+        [hud hide:YES afterDelay:1.0];
+    }
+}
+
+
+
+
+
+@end
